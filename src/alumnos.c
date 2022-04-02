ae5b214e7d9b23d4f3b46fea05a7db42dd2ecee9
EmmaJordan <ejjordan91@gmail.com>
2022-04-02 19:15:39 -0300

    Se agregan los datos del alumno JORDAN, Emmanuel

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s EMMANUEL_JORDAN = {
+        .apellidos = "JORDAN",
+        .nombres = "Joan Emmanuel",
+        .documento = "36.837.828",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &EMMANUEL_JORDAN,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
