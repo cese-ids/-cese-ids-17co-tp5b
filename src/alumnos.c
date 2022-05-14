88ef1b02dab50d443e89a2e077f005b22054f7a9
Lucas C <lconstantino@skyloom.co>
2022-05-13 21:19:45 -0300

    Se agregan los datos del alumno CONSTANTINO, Lucas

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s LUCAS_CONSTANTINO = {
+    .apellidos = "CONSTANTINO",
+    .nombres = "Lucas",
+    .documento = "36.159.943",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &LUCAS_CONSTANTINO,
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
